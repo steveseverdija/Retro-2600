78b7394b15c3c9ccbba5c88b46cbb5b6d21f4b50
Steve Severdija <steve.sever@live.ca>
2026-07-29 08:10:26 -0400

    chore: bump version to 1.5
    Update versionCode to 6 and versionName to 1.5.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.retrogame.cecfbd"
     minSdk = 24
     targetSdk = 36
-    versionCode = 5
-    versionName = "1.4"
+    versionCode = 6
+    versionName = "1.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
