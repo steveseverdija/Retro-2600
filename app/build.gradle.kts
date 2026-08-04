d14d30c3e93cf73120cce85428b7e2c66d1a51c2
Steve Severdija <steve.sever@live.ca>
2026-08-04 08:40:25 -0400

    1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.retrogame.cecfbd"
     minSdk = 24
     targetSdk = 36
-    versionCode = 8
-    versionName = "1.7"
+    versionCode = 9
+    versionName = "1.8"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
